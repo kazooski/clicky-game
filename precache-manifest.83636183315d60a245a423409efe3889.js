cace4451c08a06f4e3e5aff9bf7c927eff504605
noisette <kasusky.music@gmail.com>
2020-09-20 02:37:30 -0700

    Updates

---
+++ precache-manifest.83636183315d60a245a423409efe3889.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29af9edcba3f3f8af6f1bc999a59aeca",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "44bb60eeaf2ef2b15cfb",
+    "url": "/clicky-game/static/css/main.c1e2c7b8.chunk.css"
+  },
+  {
+    "revision": "77ec5a0aa6834a00ede5",
+    "url": "/clicky-game/static/js/2.69b5d891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.69b5d891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44bb60eeaf2ef2b15cfb",
+    "url": "/clicky-game/static/js/main.55c99ca6.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  }
+]);
